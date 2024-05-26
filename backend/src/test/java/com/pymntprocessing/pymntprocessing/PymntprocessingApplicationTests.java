0a5d4d693976513b0db3a5db5a3265faf8bec5de
behe95 <abid.ahsanahmed@gmail.com>
2024-05-26 12:05:22 -0400

    project init

---
+++ backend/src/test/java/com/pymntprocessing/pymntprocessing/PymntprocessingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pymntprocessing.pymntprocessing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PymntprocessingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
